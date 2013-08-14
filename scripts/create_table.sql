c409db033115115766e7a58febb9bc589687bb03
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-08-14 22:13:45 +0800

    Added more default data and timestamp_called for queues

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -1,9 +1,15 @@
+DROP TABLE queues;
+DROP TABLE providers;
+DROP TABLE managers;
+DROP TABLE users;
+
 CREATE TABLE users (
        id SERIAL PRIMARY KEY,
        email text NOT NULL,
        name text NOT NULL,
        password text NOT NULL
 );
+INSERT INTO users (email, name, password) VALUES ('js@world.com', 'Jie Sheng', '7c211433f02071597741e6ff5a8ea34789abbf43');
 
 CREATE TABLE managers (
        id SERIAL PRIMARY KEY,
@@ -17,10 +23,15 @@ CREATE TABLE providers (
        name text NOT NULL,
        slot int NOT NULL
 );
+INSERT INTO providers (name, slot) VALUES
+       ('Chams Clinic', 1),
+       ('Silver Cross Clinic', 2);
+);
 
 CREATE TABLE queues (
        id SERIAL PRIMARY KEY,
        provider_id int REFERENCES providers(id),
        user_id int REFERENCES users(id),
-       datetime TIMESTAMP WITH TIME ZONE
+       datetime TIMESTAMP WITH TIME ZONE,
+       datetime_called TIMESTAMP WITH TIME ZONE
 );
