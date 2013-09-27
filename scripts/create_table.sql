fccba64468b4836b99c5a8fc498e47acd62d95b3
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-09-28 03:17:44 +0800

    Added mobile to the sql table

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -7,11 +7,12 @@ CREATE TABLE users (
        id SERIAL PRIMARY KEY,
        email text NOT NULL,
        name text NOT NULL,
+       mobile text NOT NULL,
        password text NOT NULL
 );
-INSERT INTO users (email, name, password) VALUES
-       ('js@world.com', 'Jie Sheng', '7c211433f02071597741e6ff5a8ea34789abbf43'),
-       ('js@hello.com', 'Jason', 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d');
+INSERT INTO users (email, name, mobile, password) VALUES
+       ('js@world.com', 'Js Worldson', '+6597520245', '7c211433f02071597741e6ff5a8ea34789abbf43'),
+       ('js@hello.com', 'Helloson', '+6597520245', 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d');
 
 CREATE TABLE providers (
        id SERIAL PRIMARY KEY,
@@ -29,7 +30,8 @@ CREATE TABLE managers (
        provider_id int REFERENCES providers(id)
 );
 INSERT INTO managers (username, name, password, provider_id) VALUES
-       ('chams', 'Dr Chams', '053ad7fc22c565a5f386774e51b8f7782799d0aa', 1);
+       ('chams', 'Dr Chams', '053ad7fc22c565a5f386774e51b8f7782799d0aa', 1),
+       ('silver', 'Dr Silver', 'f8248e12727710c946f73d8f6e02eb93530dd9de', 2);
 
 CREATE TABLE queues (
        id SERIAL PRIMARY KEY,
