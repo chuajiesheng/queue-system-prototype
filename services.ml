a88beb5fec57a54f3e78bad9285e9a13dd9f0606
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-10-19 13:48:39 +0800

    Open Eliom_service package to ensure consistent package usage

--- services.ml
+++ services.ml
@@ -1,24 +1,26 @@
+open Eliom_service
+
 let main_service =
-  Eliom_service.service ~path:[] ~get_params:Eliom_parameter.unit ()
+  service ~path:[] ~get_params:Eliom_parameter.unit ()
 
 let login_service =
-  Eliom_service.service ~path:["login"] ~get_params:Eliom_parameter.unit ()
+  service ~path:["login"] ~get_params:Eliom_parameter.unit ()
 
 let auth_service =
-  Eliom_service.post_coservice'
+  post_coservice'
     ~post_params:Eliom_parameter.(string "username" ** string "password") ()
 
 let oauth_service =
-  Eliom_service.post_coservice'
+  post_coservice'
     ~post_params:Eliom_parameter.(string "email" **
                                     (string "name" **
                                        (string "mobile" ** string "id"))) ()
 
 let sign_up_service =
-  Eliom_service.service ~path:["signup"] ~get_params:Eliom_parameter.unit ()
+  service ~path:["signup"] ~get_params:Eliom_parameter.unit ()
 
 let create_account_service =
-  Eliom_service.post_coservice
+  post_coservice
     ~fallback:login_service
     ~name:"create_account_service"
     ~post_params:Eliom_parameter.(string "username" **
@@ -27,15 +29,15 @@ let create_account_service =
                                           (string "password" ** string "password2")))) ()
 
 let fb_login_service =
-  Eliom_service.service ~path:["fb"] ~get_params:Eliom_parameter.unit ()
+  service ~path:["fb"] ~get_params:Eliom_parameter.unit ()
 
 let menu_service =
-  Eliom_service.service ~path:["menu"] ~get_params:Eliom_parameter.unit ()
+  service ~path:["menu"] ~get_params:Eliom_parameter.unit ()
 
 let provider_service =
-  Eliom_service.service
+  service
     ~path:["provider"] ~get_params:Eliom_parameter.(suffix (string "provider")) ()
 
 let manager_service =
-  Eliom_service.service
+  service
     ~path:["manager"] ~get_params:Eliom_parameter.(suffix (string "manager")) ()
